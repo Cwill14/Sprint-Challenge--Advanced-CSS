c4e4d67ff33964b9efee31ae205176564a2a31f7
Clark Williams <theclarkwilliams@gmail.com>
2019-05-03 14:34:18 -0600

    got it fully working!

--- javascript/script1.js
+++ javascript/script1.js
@@ -2,5 +2,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 96.8 || document.documentElement.scrollTop > 96.8) {
         document.getElementById("nav-container").style.opacity = "0.75";
+    } else {
+        document.getElementById("nav-container").style.opacity = "1";
     }
 }
