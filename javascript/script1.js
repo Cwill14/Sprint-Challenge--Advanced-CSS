c087b351259a704f2f9c330533bc370474a258ea
Clark Williams <theclarkwilliams@gmail.com>
2019-05-03 14:28:32 -0600

    opacity function works only first time, if scrolled back to top it doesn't do it again

---
+++ javascript/script1.js
@@ -0,0 +1,6 @@
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 96.8 || document.documentElement.scrollTop > 96.8) {
+        document.getElementById("nav-container").style.opacity = "0.75";
+    }
+}
